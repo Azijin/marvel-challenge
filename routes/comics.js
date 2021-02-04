be4eca33f82c69d8e06c3ed280578395513c972a
Arthur Chen <arthur@MacBook-Pro-de-Arthur.local>
2021-02-04 22:39:26 +0100

    put the apiKey in an const for more visibiliy

--- routes/comics.js
+++ routes/comics.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const axios = require("axios");
 
 const urlApiMarvel = process.env.MARVEL_API_URL;
+const apiKey = process.env.MARVEL_API_KEY;
 
 router.get("/comics", async (req, res) => {
   try {
@@ -10,7 +11,7 @@ router.get("/comics", async (req, res) => {
     const title = req.query.title ? req.query.title : "";
     const limit = req.query.limit ? req.query.limit : 100;
     const callApiMarvel = await axios.get(
-      `${urlApiMarvel}/comics?apiKey=${process.env.MARVEL_API_KEY}&title=${title}&limit=${limit}&skip=${skip}`
+      `${urlApiMarvel}/comics?apiKey=${apiKey}&title=${title}&limit=${limit}&skip=${skip}`
     );
 
     if (callApiMarvel.status === 200) {
@@ -29,7 +30,7 @@ router.get("/comics/:id", async (req, res) => {
       res.status(400).json({ message: "Missing id params" });
     } else {
       const callApiMarvel = await axios.get(
-        `${urlApiMarvel}/comics/${req.params.id}?apiKey=${process.env.MARVEL_API_KEY}`
+        `${urlApiMarvel}/comics/${req.params.id}?apiKey=${apiKey}`
       );
       console.log(callApiMarvel);
       if (callApiMarvel.status === 200) {
