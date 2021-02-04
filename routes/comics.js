fdb9b1becd7e8cc64519ed762aca61a3e0b584e2
Arthur Chen <arthur@MacBook-Pro-de-Arthur.local>
2021-02-04 21:45:16 +0100

    add comic routes : get with params id who match a mongodb  document id of Character and hide the url api

--- routes/comics.js
+++ routes/comics.js
@@ -2,19 +2,48 @@ const express = require("express");
 const router = express.Router();
 const axios = require("axios");
 
+const urlApiMarvel = "https://lereacteur-marvel-api.herokuapp.com";
+
 router.get("/comics", async (req, res) => {
   try {
     const { skip } = req.query;
     const title = req.query.title ? req.query.title : "";
     const limit = req.query.limit ? req.query.limit : 100;
     const callApiMarvel = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.MARVEL_API_KEY}&title=${title}&limit=${limit}&skip=${skip}`
+      `${urlApiMarvel}/comics?apiKey=${process.env.MARVEL_API_KEY}&title=${title}&limit=${limit}&skip=${skip}`
     );
 
     if (callApiMarvel.status === 200) {
       res.status(200).json(callApiMarvel.data);
     } else {
       res.status(400).json(callApiMarvel.statusText);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+router.get("/comics/:id", async (req, res) => {
+  try {
+    if (!req.params.id) {
+      res.status(400).json({ message: "Missing id params" });
+    } else {
+      const callApiMarvel = await axios.get(
+        `${urlApiMarvel}/comics/${req.params.id}?apiKey=${process.env.MARVEL_API_KEY}`
+      );
+      console.log(callApiMarvel);
+      if (callApiMarvel.status === 200) {
+        if (callApiMarvel.data === null) {
+          res.status(404).json({
+            message:
+              "This character does not appear in any comic book in our database",
+          });
+        } else if (callApiMarvel.data) {
+          res.status(200).json({ data: callApiMarvel.data, message: "trouvé" });
+        }
+      } else {
+        res.status(400).json(callApiMarvel);
+      }
     }
   } catch (error) {
     console.log(error);
